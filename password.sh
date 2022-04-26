95bfcf24819083b9d8cad5dddedc74aae596885a
Hamza Taoujouti <htaoujouti@integrationobjects.com>
2022-04-26 11:09:32 +0100

    read help from file

--- password.sh
+++ password.sh
@@ -3,25 +3,29 @@ if test $# -eq 0 ; then
     echo "give argument"
 else        
     case $1 in
-        -t)   echo "enter the password"
-        len="$2"        
-        if test ${#len} -ge 8 ; then
-            echo "$password" | grep -q [[:digit:]]
-            if test $? -eq 0 ; then
-                echo "$password" | grep -q [[:alpha:]]
-                if test $? -eq 0 ; then                        
-                    echo "Strong password"                            
+        -t) len="$2"   
+            if test ${#len} -ge 8 ; then
+                echo "$len" | grep -q [[:digit:]]
+                if test $? -eq 0 ; then
+                    echo "$len" | grep -q [[:alpha:]]
+                    if test $? -eq 0 ; then                        
+                        echo "Strong password"                            
+                    else
+                        echo "weak password include char" 
+                    fi
                 else
-                    echo "weak password include char" 
+                    echo "please include the numbers in password it is weak password"   
                 fi
             else
-                echo "please include the numbers in password it is weak password"   
-            fi
-        else
-            echo "password lenght should be greater than or equal 8 hence weak password"
-        fi 
+                echo "password lenght should be greater than or equal 8 hence weak password"
+            fi 
         ;;
         -v) echo "Password Tester version 2.0.1 created by Hamza Taoujouti and Mouhib Trabelsi" 
         ;;
+        -h) input="./help.txt"
+            while IFS= read -r line
+            do
+                echo "$line"
+            done < "$input"
     esac
 fi
