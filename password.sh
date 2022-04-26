76b41bf3bd3a40633e2f52362b49ea30618b0637
Hamza Taoujouti <htaoujouti@integrationobjects.com>
2022-04-26 11:09:01 +0100

    troubleshoot errors

--- password.sh
+++ password.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
-if $# < 1; then
-arg = "$1"
-case arg in
-    -t)   echo "enter the password"
-    len="$2"
-    if test $len -ge 8 ; then
-        echo "$password" | grep -q [[:digit:]]
-        if test $? -eq 0 ; then
-            echo "$password" | grep -q [[:alpha:]]
-            if test $? -eq 0 ; then                        
-                echo "Strong password"                            
+if test $# -eq 0 ; then
+    echo "give argument"
+else        
+    case $1 in
+        -t)   echo "enter the password"
+        len="$2"        
+        if test ${#len} -ge 8 ; then
+            echo "$password" | grep -q [[:digit:]]
+            if test $? -eq 0 ; then
+                echo "$password" | grep -q [[:alpha:]]
+                if test $? -eq 0 ; then                        
+                    echo "Strong password"                            
+                else
+                    echo "weak password include char" 
+                fi
             else
-                echo "weak password include char" 
+                echo "please include the numbers in password it is weak password"   
             fi
         else
-            echo "please include the numbers in password it is weak password"   
-        fi
-    else
-        echo "password lenght should be greater than or equal 8 hence weak password"
-    fi 
-    ;;
-    -v) echo "Password Tester version 2.0.1 created by Hamza Taoujouti and Mouhib Trabelsi" 
-    ;;
-esac
-else
-    echo "give argument"
-fi    
+            echo "password lenght should be greater than or equal 8 hence weak password"
+        fi 
+        ;;
+        -v) echo "Password Tester version 2.0.1 created by Hamza Taoujouti and Mouhib Trabelsi" 
+        ;;
+    esac
+fi
